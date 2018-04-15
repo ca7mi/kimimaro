8e05c16cd269081fdcaf528f45832ae7c9911905
kanami sugiyama <kanamisugiyama@kanami-no-MacBook-Pro.local>
2018-04-15 21:44:28 +0900

    Improve

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
 
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
-        console.log(`${event.message.text}  : eventMsgTxt.`);
+        console.log(`${event.source.userId}  : UserId`);
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
